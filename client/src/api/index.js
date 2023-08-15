5cba448f1aa94d426cda461dba24ce26c35e6259
Harivams Sai Gudalwar <harivamssai.gudalwar@cgi.com>
2023-08-15 23:39:17 +0530

    fixes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://eventsnap-backend.onrender.com' });
 
 API.interceptors.request.use((req) => {
-  if(localStorage.getItem('profile')) {
+  if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
   }
   return req;
